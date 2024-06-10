b9f20b26f00ef722fa757f7aeb073c8a61288d1c
Diovanio Mota <diovaniomota8@gmail.com>
2024-06-10 20:44:53 -0300

    main

--- src/modules/login/screen/LoginScreen.tsx
+++ src/modules/login/screen/LoginScreen.tsx
@@ -1,18 +1,42 @@
 import { ContainerLogin, EntradaImage, LimitedContainer ,LogoImage, ContainerLoginScreen, TitleLogin } from "../styles/loginScreen.styles"
 import Input from "../../../shared/components/inputs/input/input";
 import Button from "../../../shared/buttons/button/Button";
+import { useState } from "react";
 
 
 const LoginScreen =  () => {
+    
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    }
+
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    }
+
+    const handleLogin = () => {
+        alert({
+            username,
+            password,
+        });
+    };
+    
     return (
         <ContainerLoginScreen>
             <ContainerLogin>
                 <LimitedContainer>
                     <LogoImage src="./logo.png" />
                         <TitleLogin level={2} type="secondary">Login</TitleLogin>
-                        <Input title="Uusuário" margin="32px 0px 0px "/>
-                        <Input title="Senha" margin="32px 0px 0px"/>
-                        <Button type="primary" margin="20px 8px 16px 8px">Entrar</Button>
+                        <Input title="Uusuário" margin="32px 0px 0px " onChange={handleUsername} value={username}/>
+                        <Input
+                        type="password"
+                         title="Senha"
+                          margin="32px 0px 0px" 
+                          onChange={handlePassword} value={password}/>
+                        <Button type="primary" margin="20px 8px 16px 8px" onClick={handleLogin}>Entrar</Button>
                 </LimitedContainer>
             </ContainerLogin>
             <EntradaImage src="./entrada.png"/>
